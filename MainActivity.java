642c681f715f2ec9b940066e1b3f15cf8a2fe726
15Cyndaquil <john.tyler.gafford@gmail.com>
2017-04-17 21:17:28 -0400

    Test add, just set up project

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cyndaquil.csc_201_14291533;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
